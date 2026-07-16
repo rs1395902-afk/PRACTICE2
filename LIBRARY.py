093050002a6b4e49eb9d4a2d4203b0052021a634
MOHIT-PYTHON <rs1395902@gmail.com>
2026-07-16 10:31:27 +0530

    ISSUE FUNCTION ADDED

--- LIBRARY.py
+++ LIBRARY.py
@@ -14,5 +14,16 @@ while True:         #SHOW THE MENU BAR
         print("\nAVAILABLE BOOKS : ")
         for book in books:
             print(book)
-    elif choice == "2":
-        #ADD BOOKS
+    elif choice == "2":  #ADD BOOKS
+        new_book = input("ENTER THE NAME OF THE BOOK TO ADD : ")
+        books.append(new_book)
+        print(f"{new_book} HAS BEEN ADDED TO THE LIBRARY ")
+    elif choice == 3:  #ISSUE BOOKS
+        issue_book = input("ENTER THE BOOK NAME TO ISSUE : ")
+        if issue_book in books:
+            books.remove(issue_book)
+            print(f"{issue_book} HAS BEEN ISSUED")
+        else:
+            print("NOT AVAILABLE")
+    
+       
