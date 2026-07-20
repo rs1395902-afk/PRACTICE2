2b71bf708d000409cd6f9d402ccfde8ed51e22b1
MOHIT-PYTHON <rs1395902@gmail.com>
2026-07-20 10:42:21 +0530

    COMPLETED

--- BANKING_SYSTEM.py
+++ BANKING_SYSTEM.py
@@ -1,12 +1,41 @@
 print("\n====WELCOME TO INDIAN BANK====")
 
+balance = 25800
+
 while True:         #MENU BAAR
-    print("1. CHECK BALANCE")
-    print("2. WITHDRAWL BALANCE")
-    print("3. DEPOSIT BALANCE")
-    print("4. EXIT")
+    print("1 CHECK BALANCE")
+    print("2 WITHDRAWL BALANCE")
+    print("3 DEPOSIT BALANCE")
+    print("4 EXIT")
     
-    a = int(input("ENTER YOUR CHOICE : "))
+    a = input("ENTER YOUR CHOICE : ")
+
+    if a == "1":
+        print(f"THE AVAILABLE BALANCE ON YOUR ACCOUNT IS : {balance} ")
+    elif a == "2":
+        amt = int(input("ENTER THE AMOUNT :"))
+        balance = balance - amt
+        print(f"YOUR AMOUNT {amt} HAS BEEN SUCCESFULLY WITHDRAWN")
+        print(f"THE AVAILABLE BALANCE ON YOUR ACCOUNT IS : {balance} ")
+    elif a == "3":
+        amt = int(input("ENTER THE AMOUNT :"))
+        balance = balance + amt
+        print(f"YOUR AMOUNT {amt} HAS BEEN SUCCESFULLY DEPOSITED")
+        print(f"THE AVAILABLE BALANCE ON YOUR ACCOUNT IS : {balance} ")
+    elif a == "4":
+        print("THANK YOU FOR VISITING")
+        break
+    else:
+        print("INVAILD CHOICE")
+
+
+
+
+
+
+
+
+
     
 
     
