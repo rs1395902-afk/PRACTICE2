0523b931ed05a7f19e33a1ea89bbdaaf09b054d9
MOHIT-PYTHON <rs1395902@gmail.com>
2026-07-20 10:10:06 +0530

    ADDED MENU BAAR

---
+++ BANKING_SYSTEM.py
@@ -0,0 +1,8 @@
+print("\n====WELCOME TO INDIAN BANK====")
+
+while True:         #MENU BAAR
+    print("1. CHECK BALANCE")
+    print("2. WITHDRAWL BALANCE")
+    print("3. DEPOSIT BALANCE")
+    print("4. EXIT")
+    
